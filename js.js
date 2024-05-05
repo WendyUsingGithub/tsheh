816e05225dc40e0a3d089a6720ae12d1485d1bdb
Wendy <cutebitterblue@gmail.com>
2024-05-05 15:25:36 +0800

    update subscribe button
    combine author name and subscribe button

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
-const subscribe = document.getElementById("subscribe-icon");
+const subscribe_icon = document.getElementById("subscribe-icon");
+const subscribe_icon_vertical = document.getElementById("subscribe-icon-vertical");
+const subscribe_icon_horizontal = document.getElementById("subscribe-icon-horizontal");
 const subscribe_text = document.getElementById("subscribe-text");
 const subscribe_button = document.getElementById("subscribe-button");
 let subscribe_deg = 0;
@@ -8,22 +10,25 @@ let click = 0;
 subscribe_button.addEventListener("mouseenter", () =>
 {
   subscribe_deg = subscribe_deg + 90;
-  subscribe.style.transform = "rotate(" + subscribe_deg + "deg)";
+  subscribe_icon.style.transform = "rotate(" + subscribe_deg + "deg)";
+  subscribe_icon.style.transition = "all 0.3s ease-in-out";
+  subscribe_text.style.color = "var(--tsheh-gray)";
+  subscribe_icon_vertical.style.background = "var(--tsheh-gray)";
+  subscribe_icon_horizontal.style.background = "var(--tsheh-gray)";
 });
 
 subscribe_button.addEventListener("click", () =>
 {
   subscribe_deg = subscribe_deg + 135;
-  subscribe.style.transform = "rotate(" + subscribe_deg + "deg)";
+  subscribe_icon.style.transform = "rotate(" + subscribe_deg + "deg)";
+  subscribe_icon.style.transition = "all 0.45s ease-in-out";
 
   if(subscribe_state == 0)
   {
-    subscribe_text.innerText = "已訂閱";
     subscribe_state = 1;
   }
   else
   {
-    subscribe_text.innerText = "訂閱";
     subscribe_state = 0; 
   }
 
@@ -36,7 +41,11 @@ subscribe_button.addEventListener("mouseleave", () =>
   if(click == 0)
   {
     subscribe_deg = subscribe_deg - 90;
-    subscribe.style.transform = "rotate(" + subscribe_deg + "deg)";
+    subscribe_icon.style.transform = "rotate(" + subscribe_deg + "deg)";
+    subscribe_icon.style.transition = "all 0.3s ease-in-out";
+    subscribe_text.style.color = "var(--tsheh-black)";
+    subscribe_icon_vertical.style.background = "var(--tsheh-black)";
+    subscribe_icon_horizontal.style.background = "var(--tsheh-black)";
   }
 
   click = 0;
